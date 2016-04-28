d9620f3863f6edd84e214554c3ea23a683b93d09
Mohammed Aizaaz Ali <mohammed.ali@nyu.edu>
2016-04-27 22:12:30 -0400

    Added files via upload

---
+++ get_article.py
@@ -0,0 +1,49 @@
+'''Script name : get_article.py
+Authors         : Abhishek kadian and Mohammed Aizaaz Ali
+Created        : April 12, 2016
+Last Modified  : April 27,2016
+Version        : 1.0
+Description    : scrape news articles from links stored in a file'''
+
+import time
+import requests
+import io
+from newspaper import fulltext
+from user_agent import generate_user_agent
+
+def getarticle(readfile):
+    ''' get the article and save it in a different file '''
+    try:
+        fileopen = open(readfile)
+    except IOError:
+        print "file " + readfile + " not in the location specified"
+        return
+
+    i = 1
+    for line in fileopen:
+        try:
+        	ua = generate_user_agent()
+        	head = ua.encode('ascii', 'ignore')
+        	headers = {'useragent':head}
+
+        	print "reading article :"
+        	print line
+        	html = requests.get(line, headers = headers).text
+        	tex = fulltext(html)
+        	writefile = "201602"+str(j)+"_"+str(i)+".txt"
+        	with io.open(writefile, encoding='utf-8', mode='w+') as ns:
+        		strng = ' '.join(tex.split())
+        		ns.write(strng)
+        		ns.close()
+        	i = i + 1       	
+       	except:
+       	    pass
+
+j = 1
+while j < 30:
+    filename = "links"+str(j)+".txt"
+    print "extracting data for "+filename
+    getarticle(filename)
+    j = j + 1
+
+print "successsfull"
